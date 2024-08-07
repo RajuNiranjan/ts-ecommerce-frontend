1a6eed35d143ffb9dc6bb333dcb08b67514c1b6d
RajuNiranjan <rajuniranjan1910@gmail.com>
2024-08-07 09:55:07 +0530

    removed store

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,6 @@
-import { persistor, store } from "@/store/store";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <Provider store={store}>
-      <PersistGate loading={true} persistor={persistor}></PersistGate>
-
-      <div className="bg-violet-50">
-        <Component {...pageProps} />
-      </div>
-    </Provider>
-  );
+  return <Component {...pageProps} />;
 }
