c949f43a636f8e30adfbdf9fe0955d9f751e685d
RajuNiranjan <rajuniranjan1910@gmail.com>
2024-08-09 14:11:27 +0530

    updated store with redux-persist

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,19 @@
 import { AppProps } from "next/app";
 import { Provider } from "react-redux";
-// import { PersistGate } from "redux-persist/integration/react";
-import { store } from "@/store/store"; // Adjust the path as necessary
+import { PersistGate } from "redux-persist/integration/react";
+import { persistor, store } from "@/store/store"; // Adjust the path as necessary
 import "../styles/globals.css";
 import NavBar from "@/components/NavBar";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
-      {/* <PersistGate loading={null} persistor={persistor}> */}
-      <div className="bg-[#BC9179]">
-        <NavBar />
-        <Component {...pageProps} />
-      </div>
-      {/* </PersistGate> */}
+      <PersistGate loading={null} persistor={persistor}>
+        <div className="bg-[#BC9179]">
+          <NavBar />
+          <Component {...pageProps} />
+        </div>
+      </PersistGate>
     </Provider>
   );
 }
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,45 +1,33 @@
-// import { configureStore } from "@reduxjs/toolkit";
-// import { combineReducers } from "redux";
-// import { persistReducer, persistStore } from "redux-persist";
-// import storage from "redux-persist/lib/storage";
-// import userReducer from "./Actions/userSlice.action"; // Adjust the path as necessary
-
-// const rootReducer = combineReducers({
-//   user: userReducer,
-// });
-
-// const persistConfig = {
-//   key: "root",
-//   storage,
-//   version: 1,
-// };
-
-// const persistedReducer = persistReducer(persistConfig, rootReducer);
-
-// export const store = configureStore({
-//   reducer: persistedReducer,
-//   middleware: (getDefaultMiddleware) =>
-//     getDefaultMiddleware({
-//       serializableCheck: {
-//         ignoredActions: ["persist/PERSIST", "persist/REHYDRATE"],
-//         ignoredPaths: ["_persist"],
-//       },
-//     }),
-// });
-
-// export const persistor = persistStore(store);
-
-// export type RootState = ReturnType<typeof store.getState>;
-// export type AppDispatch = typeof store.dispatch;
-
 import { configureStore } from "@reduxjs/toolkit";
+import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
+import { combineReducers } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+
 import UserReducer from "./Actions/userSlice.action";
 
-export const store = configureStore({
-  reducer: {
-    user: UserReducer,
-  },
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["user"],
+};
+
+const rootReducer = combineReducers({
+  user: UserReducer,
 });
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: ["persist/PERSIST"],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
